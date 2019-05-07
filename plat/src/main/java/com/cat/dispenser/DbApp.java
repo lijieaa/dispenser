0863c7b5979fcd04bda3b7339d3abbd74e12cc63
jay <9520547672@qq.com>
2019-05-07 18:03:57 +0800

    qrcode

--- plat/src/main/java/com/cat/dispenser/DbApp.java
+++ plat/src/main/java/com/cat/dispenser/DbApp.java
@@ -4,12 +4,17 @@ package com.cat.dispenser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @SpringBootApplication
 @Controller
+@RequestMapping("f")
 public class DbApp {
 
     public static void main(String[] args) {
@@ -17,9 +22,26 @@ public class DbApp {
     }
 
 
-    @RequestMapping("test")
-    @ResponseBody
-    String test(){
-        return "xxxxxx";
+
+    @RequestMapping(value = "goods",method = RequestMethod.GET)
+    String goods(Model model){
+
+        return "f/goods";
+    }
+
+
+
+    @RequestMapping(value = "pay",method = RequestMethod.GET)
+    String pay(HttpServletRequest request,Model model){
+        String userAgent = request.getHeader("user-agent");
+        if (userAgent != null && userAgent.contains("AlipayClient")) {
+            model.addAttribute("flag",true);
+            return "/f/pay";
+        }else if (userAgent != null && userAgent.contains("MicroMessenger")) {
+            model.addAttribute("flag",false);
+            return "/f/pay";
+        }else{
+            return "/f/pay_error";
+        }
     }
 }
